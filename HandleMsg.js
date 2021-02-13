b335149ca2c8c930afb415d51a56c430d34d5572
Nur Aziz <naziz0404@gmail.com>
2021-02-13 20:19:01 +0700

    change package & sticker process

--- HandleMsg.js
+++ HandleMsg.js
@@ -404,11 +404,11 @@ module.exports = HandleMsg = async (client, message) => {
               const _mimetype = isQuotedImage ? quotedMsg.mimetype : mimetype;
               const mediaData = await decryptMedia(encryptMedia, uaOverride);
               const imageBase64 = `data:${_mimetype};base64,${mediaData.toString("base64")}`;
-              var outFilest = './media/sticker.jpg'
+              //var outFilest = './media/sticker.jpg'
               //await fs.writeFile(outFilest, imageBase64, "base64")
               //const getUrl = await uploadImagesV2(outFilest, false);
               //console.log(getUrl);
-              client.sendImageAsSticker(from, outFilest, stickerMetadata)
+              client.sendImageAsSticker(from, imageBase64, stickerMetadata)
                 .then(() => {
                   client.sendText(from, "Here's your sticker");
                   console.log(`Sticker Processed for ${processTime(t, moment())} Second`);
